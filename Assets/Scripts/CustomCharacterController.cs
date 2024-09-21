31dd7083c2d409f95847c846cf56a799fcd86822
Dominic-Emond <emond.dominic@gmail.com>
2024-09-20 23:01:36 -0400

    Better Movement

--- Assets/Scripts/CustomCharacterController.cs
+++ Assets/Scripts/CustomCharacterController.cs
@@ -1,47 +1,79 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
+/// <summary>
+/// Character Controller, takes care of the player movement.
+/// </summary>
 public class CustomCharacterController : MonoBehaviour
 {
     [Header("Movement Dials")]
     [SerializeField] private int moveSpeed = 5;
 
     [SerializeField] private int jumpStrength = 5;
+
+    [Header("Character Controls")] 
+    public InputAction controls;
     
+    
+    //Private fields
     private Rigidbody2D _rigidBody;
+    private bool _isGrounded;
 
     void Start()
     {
         //Initializing Variables
+        _isGrounded = true; // (!) Change
         _rigidBody = GetComponent<Rigidbody2D>();
     }
-    
-    void FixedUpdate()
+
+    /// <summary>
+    /// When GameObject is enabled, starts the InputAction Controls.
+    /// Important to avoid issue (?)
+    /// </summary>
+    private void OnEnable()
     {
-        //Basic Movement. Change for Input Map and smoother movement. Also Change Velocity Instead?
-        Vector3 movement = new Vector3(0, 0, 0);
-        
-        if (Input.GetKey(KeyCode.A)) //Left
-        {
-            movement.x -= moveSpeed * Time.fixedDeltaTime;
-        }
-
-        if (Input.GetKey(KeyCode.D)) //Right
-        {
-            movement.x += moveSpeed * Time.fixedDeltaTime;
-        }
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            Jump();
-        }
-
-        transform.position += movement;
+        controls.Enable();
     }
 
-    private void Jump()
+    /// <summary>
+    /// When GameObject is disabled, disables the InputAction Controls.
+    /// </summary>
+    private void OnDisable()
     {
-      _rigidBody.velocity = new Vector2(0, jumpStrength);
+        controls.Disable();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            _isGrounded = true;
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        //Vector for character movement
+        //X value based on player Input
+        Vector2 movement = new Vector2();
+        movement.x = controls.ReadValue<Vector2>().x * Time.fixedDeltaTime * moveSpeed;
+        
+        //Y mixed between Jumping and Gravity
+        if (Input.GetKey(KeyCode.Space) && _isGrounded)
+        {
+            _isGrounded = false;
+            movement.y = jumpStrength;
+        }
+        else
+        {
+            movement.y = _rigidBody.velocity.y;
+        }
+        
+        _rigidBody.velocity = movement;
     }
 }
