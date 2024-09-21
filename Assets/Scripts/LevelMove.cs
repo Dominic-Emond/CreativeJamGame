a425f5f3329aab88c2ca40b883656577021884d0
DayDreamer027 <DayDreamer030906@gmail.com>
2024-09-21 14:05:17 -0400

    Added LevelMove to Platforms for Testing

--- Assets/Scripts/LevelMove.cs
+++ Assets/Scripts/LevelMove.cs
@@ -6,7 +6,7 @@ public class LevelMove : MonoBehaviour
 {
    
     [SerializeField] private Vector3 location;
-    [SerializeField] private float moveSpeed;
+    [SerializeField] private float moveSpeed = 1;
     bool finished = false;
     // Start is called before the first frame update
     void Start()
